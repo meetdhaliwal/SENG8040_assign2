16e9cf8484b2ef714f3c48004da2794d88b42193
meetdhaliwal <50705838+meetdhaliwal@users.noreply.github.com>
2019-06-03 18:24:12 -0400

    Add files via upload

---
+++ triangleproject/triangleproject/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangleproject
+{
+    class Program
+    {
+
+
+
+        public static int ProgramMenu()
+        {
+            string inputValue = string.Empty;
+            bool ValidSelection = false;
+            while (ValidSelection == false)
+            {
+                Console.WriteLine("1. Enter triangle dimentions.");
+                Console.WriteLine(". Exit");
+                inputValue = Console.ReadLine();
+                if (inputValue != "1" && inputValue != "2")
+                {
+                    Console.WriteLine("\n");
+                }
+                else
+                {
+                    ValidSelection = true;
+                }
+            }
+            Console.WriteLine();
+            return int.Parse(inputValue);
+
+            public string INVal()
+            {
+                int part1;
+                int part2;
+                int part3;
+
+                Console.Write("Enter the first side of the triangle: ");
+                part1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter the second side of the triangle: ");
+                part2 = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter the third side of the triangle: ");
+                part3 = int.Parse(Console.ReadLine());
+
+                TriangleSolver.Analyze(part1, part2, part3);
+            } }
+
+           public static void Main(string[] args)
+            {
+               
+    }
+        
+    } }
---
+++ triangleproject/triangleproject/TriangleSolver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangleproject
+{
+    public static class TriangleSolver
+    {
+
+
+        public static string Analyze(int part1, int part2, int part3)
+        {
+            string x = string.Empty;
+            if (part1 + part2 > part3 && part2 + part3 > part1 && part1 + part3 > part2)
+            {
+                Console.WriteLine("Values you entered forms a triangle.");
+            }
+            else if (part1 == part2 && part2 == part3)
+            {
+                Console.Write("It is an equilateral triangle\n");
+            }
+            else if (part1 == part2 || part2 == part3 || part1 == part3)
+            {
+                Console.Write("It is an isosceles triangle\n");
+            }
+            else
+            {
+                Console.Write("It is an scalene triangle\n");
+            }
+            
+        
+            
+            return x;
+        }
+
+    }
+    } 
+
+    
+
